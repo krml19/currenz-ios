5fe2f3497764006f112dae197e1993f5b83ce209
Marcin Karmelita <karmelita.marcin.mk@gmail.com>
2018-08-02 23:07:42 +0200

    Remove shadow rasterisation

--- Currenz/Extensions/UIView+Shadow.swift
+++ Currenz/Extensions/UIView+Shadow.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 extension UIView {
-    func dropShadow(width: CGFloat = 1, color: UIColor = UIColor.black.withAlphaComponent(0.5), radius: CGFloat = 5, offset: CGSize = CGSize(width: 0, height: 3), opacity: Float = 0.5, shouldRasterize: Bool = true) {
+    func dropShadow(width: CGFloat = 1, color: UIColor = UIColor.black.withAlphaComponent(0.5), radius: CGFloat = 5, offset: CGSize = CGSize(width: 0, height: 3), opacity: Float = 0.5, shouldRasterize: Bool = false) {
         layer.shadowColor = color.cgColor
         layer.shadowRadius = radius
         layer.cornerRadius = radius
